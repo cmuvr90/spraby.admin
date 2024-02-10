dc7f15c7a6ed3bb5372efed38ec3fdc0acf409c0
cmuvr <smuvr90@gmail.com>
2024-02-10 16:27:32 +0100

    add users table

--- theme/templates/AdminUsers.tsx
+++ theme/templates/AdminUsers.tsx
@@ -1,7 +1,52 @@
 'use client'
 
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Table, Tag} from 'antd';
+import type {TableProps} from 'antd';
+import {UsersModel} from "@/prisma/db.client";
+import {getUsersList} from "@/services/Users";
 
 export default function AdminUsers() {
-  return <>USERS</>
+  const [users, setUsers] = useState<UsersModel[]>([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    onGetUsers().then()
+  }, []);
+
+  const onGetUsers = async () => {
+    setLoading(true)
+    const users = await getUsersList()
+    setUsers(users)
+    setLoading(false)
+  }
+
+  return <Table
+    loading={loading}
+    columns={columns}
+    dataSource={users}
+    pagination={false}
+  />
 }
+
+const columns: TableProps['columns'] = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (name: string) => name ?? '-',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Role',
+    dataIndex: 'role',
+    key: 'role',
+    render: (role) => <Tag color={'blue'}>
+      {role.toUpperCase()}
+    </Tag>
+  }
+];
